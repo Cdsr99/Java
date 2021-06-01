33dcf9e661fa32afb04adbd08166bdf0fcc02b69
Caio da Silveira Rodrigues <cdsr073@gmail.com>
2021-06-01 18:51:04 -0300

    Update Index.java

--- Figura_Geometrica/src/cdsr99/com/github/Figura_Geometrica/programas/Index.java
+++ Figura_Geometrica/src/cdsr99/com/github/Figura_Geometrica/programas/Index.java
@@ -14,122 +14,160 @@ public class Index {
 		// TODO Auto-generated method stub
 		Triangulo tri = new Triangulo();
 		
+		int ciclo = 1;
+		int continu;
 		
-		System.out.println("************ Welcome ***************");
-		System.out.println("* Please, choose one:              *");
-		System.out.println("* 1) Triangule                     *");
-		System.out.println("* 2) Square                        *");
-		System.out.println("* 3) Rectangle                     *");
-		System.out.println("* 4) Exit                          *");
-		System.out.println("*************************************");
-		System.out.println();
-		
-		Scanner scan = new Scanner(System.in);
-		
-		int chose = scan.nextInt();
-		
-		switch(chose) {
-		case 1:
-			Triangulo fig = new Triangulo();
-			
-			Float l1;
-			Float l2;
-			Float l3;
-			
-			Float base;
-			Float height;
-			
-			//System.out.println("A área do triangulo é: "+ fig.CalcularL2(5f, 23f));
-			System.out.println("Enter with the side of the triangule");
+		do {
+			System.out.println();
+			System.out.println("************ Welcome ***************");
+			System.out.println("* Please, choose one:              *");
+			System.out.println("* 1) Triangule                     *");
+			System.out.println("* 2) Square                        *");
+			System.out.println("* 3) Rectangle                     *");
+			System.out.println("* 4) Exit                          *");
+			System.out.println("*************************************");
 			System.out.println();
 			
-			System.out.println("side 1:");
-			System.out.println();
-			l1 = scan.nextFloat();
-			tri.setLado1(l1);
+			Scanner scan = new Scanner(System.in);
 			
-			System.out.println("side 2:");
-			System.out.println();
-			l2 = scan.nextFloat();
-			tri.setLado2(l2);
+			int chose = scan.nextInt();
 			
-			System.out.println("side 3:");
-			System.out.println();
-			l3 = scan.nextFloat();
-			tri.setLado3(l3);
-			
-			System.out.println("Please, enter with the base:");
-			System.out.println();
-			base = scan.nextFloat();
-			tri.setBase(base);
-			
-			System.out.println("Please, enter with the height:");
-			System.out.println();
-			height = scan.nextFloat();
-			tri.setHeight(height);
-			
-			System.out.println("Triangule Status: ");
-			System.out.println();
-			System.out.println("Area: "+ tri.CalcularArea());
-			System.out.println();
-			System.out.println("Perimeter: "+ tri.CalcularPerimetro());
-			break;
-			
-		case 2:
-			
-			Quadrado FigQuadrado = new Quadrado();
-			
-			Float la1;
+			switch(chose) {
+			case 1:
+				Triangulo fig = new Triangulo();
+				
+				Float l1;
+				Float l2;
+				Float l3;
+				
+				Float base;
+				Float height;
+				
+				//System.out.println("A área do triangulo é: "+ fig.CalcularL2(5f, 23f));
+				System.out.println("Enter with the side of the triangule");
+				System.out.println();
+				
+				System.out.println("side 1:");
+				System.out.println();
+				l1 = scan.nextFloat();
+				tri.setLado1(l1);
+				
+				System.out.println("side 2:");
+				System.out.println();
+				l2 = scan.nextFloat();
+				tri.setLado2(l2);
+				
+				System.out.println("side 3:");
+				System.out.println();
+				l3 = scan.nextFloat();
+				tri.setLado3(l3);
+				
+				System.out.println("Please, enter with the base:");
+				System.out.println();
+				base = scan.nextFloat();
+				tri.setBase(base);
+				
+				System.out.println("Please, enter with the height:");
+				System.out.println();
+				height = scan.nextFloat();
+				tri.setHeight(height);
+				
+				System.out.println("Triangule Status: ");
+				System.out.println();
+				System.out.println("Area: "+ tri.CalcularArea());
+				System.out.println();
+				System.out.println("Perimeter: "+ tri.CalcularPerimetro());
+				
+				System.out.println();
+				System.out.println("Do you would like to continue? <1>Yes <2> No");
+				continu = scan.nextInt();
+				if(continu == 1) {
+					ciclo = 1;
+				}else {
+					ciclo = 0;
+				}
+				
+				
+				break;
+				
+			case 2:
+				
+				Quadrado FigQuadrado = new Quadrado();
+				
+				Float la1;
 
+				
+				//System.out.println("A área do triangulo é: "+ fig.CalcularL2(5f, 23f));
+				System.out.println("Enter with the side of the square");
+				System.out.println();
+				
+				System.out.println("side 1:");
+				System.out.println();
+				la1 = scan.nextFloat();
+				FigQuadrado.setL1(la1);
+				
+				
+				System.out.println("Square Status: ");
+				System.out.println();
+				System.out.println("Area: "+ FigQuadrado.CalcularArea());
+				System.out.println();
+				System.out.println("Perimeter: "+ FigQuadrado.CalcularPerimetro());
+				
+				System.out.println();
+				System.out.println("Do you would like to continue? <1>Yes <2> No");
+				continu = scan.nextInt();
+				if(continu == 1) {
+					ciclo = 1;
+				}else {
+					ciclo = 0;
+				}
+				
+				break;
+				
+			case 3:
+				
+				Retangulo FigRetangulo = new Retangulo();
+				
+				System.out.println("Enter with the data of the retangulo");
+				System.out.println();
+				
+				System.out.println("Width:");
+				System.out.println();
+				la1 = scan.nextFloat();
+				FigRetangulo.setW(la1);
+				System.out.println();
+				
+				System.out.println("length:");
+				System.out.println();
+				la1 = scan.nextFloat();
+				FigRetangulo.setL(la1);
+				
+				
+				System.out.println("Retangulo Status: ");
+				System.out.println();
+				System.out.println("Area: "+ FigRetangulo.CalcularArea());
+				System.out.println();
+				System.out.println("Perimeter: "+ FigRetangulo.CalcularPerimetro());
+				
+				System.out.println();
+				System.out.println("Do you would like to continue? <1>Yes <2> No");
+				continu = scan.nextInt();
+				if(continu == 1) {
+					ciclo = 1;
+				}else {
+					ciclo = 0;
+				}
+				break;
+				
+			case 4:
+				ciclo = 0;
+				//System.exit(0);
+				break;
+			}
 			
-			//System.out.println("A área do triangulo é: "+ fig.CalcularL2(5f, 23f));
-			System.out.println("Enter with the side of the square");
-			System.out.println();
-			
-			System.out.println("side 1:");
-			System.out.println();
-			la1 = scan.nextFloat();
-			FigQuadrado.setL1(la1);
-			
-			
-			System.out.println("Square Status: ");
-			System.out.println();
-			System.out.println("Area: "+ FigQuadrado.CalcularArea());
-			System.out.println();
-			System.out.println("Perimeter: "+ FigQuadrado.CalcularPerimetro());
-			
-			break;
-			
-		case 3:
-			
-			Retangulo FigRetangulo = new Retangulo();
-			
-			System.out.println("Enter with the data of the retangulo");
-			System.out.println();
-			
-			System.out.println("Width:");
-			System.out.println();
-			la1 = scan.nextFloat();
-			FigRetangulo.setW(la1);
-			System.out.println();
-			
-			System.out.println("length:");
-			System.out.println();
-			la1 = scan.nextFloat();
-			FigRetangulo.setL(la1);
-			
-			
-			System.out.println("Retangulo Status: ");
-			System.out.println();
-			System.out.println("Area: "+ FigRetangulo.CalcularArea());
-			System.out.println();
-			System.out.println("Perimeter: "+ FigRetangulo.CalcularPerimetro());
-			break;
-			
-		case 4:
-			System.exit(0);
-			break;
-		}
+		}while(ciclo == 1);
+		
+
 		
 	}
 
