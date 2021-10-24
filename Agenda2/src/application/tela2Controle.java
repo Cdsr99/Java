85757eb4be19dabfe5c24615cc19a61c14d27f12
Caio da Silveira Rodrigues <31573978+Cdsr99@users.noreply.github.com>
2021-10-24 17:06:13 -0300

    Little project based on JavaFX, this is a simple screen switching program v1
    v1

---
+++ Agenda2/src/application/tela2Controle.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class tela2Controle implements Initializable{
+	
+	
+	
+		private Stage stage;
+		private Scene scene;
+		private Parent root;
+		public Button criar; 
+		
+		public void vaiParaTela1(ActionEvent event) {
+			try {
+				root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+				stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+				scene  = new Scene(root,500,600);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				stage.setScene(scene);
+				stage.show();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+
+
